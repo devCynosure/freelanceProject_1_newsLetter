4e8074153caf758da8fc11c4c1005fecd91d3dcb
shayan user <shayanprakash@personal.server1.com>
2021-07-01 23:31:33 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+res.end("<a href=\"www.google.com\"> Google</a>");
+});
+
+
+app.listen(3000, function(){
+console.log("listening on port 3000");
+}); 
